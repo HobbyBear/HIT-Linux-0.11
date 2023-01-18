7c19b0387881180017a0bd2c662781faf61c5bc0
xiongch <xiongch@flatincbr.com>
2023-01-18 22:07:57 +0800

    fix

--- 0-prepEnv/hit-oslab-qiuyu/oslab/linux-0.11/kernel/chr_drv/console.c
+++ 0-prepEnv/hit-oslab-qiuyu/oslab/linux-0.11/kernel/chr_drv/console.c
@@ -53,7 +53,6 @@
 
 #define NPAR 16
 
-extern void keyboard_interrupt(void);
 
 static unsigned char	video_type;		/* Type of display being used	*/
 static unsigned long	video_num_columns;	/* Number of text columns	*/
@@ -680,7 +679,6 @@ void con_init(void)
 	bottom	= video_num_lines;
 
 	gotoxy(ORIG_X,ORIG_Y);
-	set_trap_gate(0x21,&keyboard_interrupt);
 	outb_p(inb_p(0x21)&0xfd,0x21);
 	a=inb_p(0x61);
 	outb_p(a|0x80,0x61);
